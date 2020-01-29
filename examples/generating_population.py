80139f24e66d9d32adf0d34e6ad340782ded729a
MicheleRonchi <ronchi@ice.csic.es>
2020-01-29 11:20:50 +0100

    Add the proper velocity output in generating_population.py and updating the jupyter notebook with the histogram of the proper velocities

--- examples/generating_population.py
+++ examples/generating_population.py
@@ -6,15 +6,30 @@ import pypopsyn.simulator.initial_population as ipop
 
 NS_population_initial = ipop.InitialNeutronStarPopulation()
 x_initial, y_initial, z_initial = NS_population_initial.position()
+(
+    vp_x_initial,
+    vp_y_initial,
+    vp_z_initial,
+) = NS_population_initial.proper_velocity()
 
 
 # adding the coordinates to a data frame for export
 df_initial = pd.DataFrame(
-    {"x_initial": x_initial, "y_initial": y_initial, "z_initial": z_initial}
+    {
+        "x_initial": x_initial,
+        "y_initial": y_initial,
+        "z_initial": z_initial,
+        "vp_x_initial": vp_x_initial,
+        "vp_y_initial": vp_y_initial,
+        "vp_z_initial": vp_z_initial,
+    }
 )
 
 df_initial.columns = pd.MultiIndex.from_tuples(
-    zip(df_initial.columns, ["[kpc]", "[kpc]", "[kpc]"])
+    zip(
+        df_initial.columns,
+        ["[kpc]", "[kpc]", "[kpc]", "[km / s]", "[km / s]", "[km / s]"],
+    )
 )
 
 df_initial.to_csv(
