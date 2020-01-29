393ed222c3060d96352564f39ca62f3e350e963a
MicheleRonchi <ronchi@ice.csic.es>
2020-01-29 17:57:31 +0100

    Add the test function for random_from_cdf

--- pypopsyn/simulator/tests/test_cdf_calculator.py
+++ pypopsyn/simulator/tests/test_cdf_calculator.py
@@ -4,6 +4,8 @@ import pytest
 import pypopsyn.simulator.cdf_calculator as cc
 import pypopsyn.simulator.initial_position as ip
 
+TOL = 1e-5
+
 
 @pytest.fixture()
 def test_case_1():
@@ -15,9 +17,38 @@ def test_case_1():
     return data
 
 
+@pytest.fixture()
+def test_case_2():
+    data = {
+        "x": np.linspace(0.0, 10.0, 5),
+        "cdf": 1.0 / 10.0 * np.linspace(0.0, 10.0, 5),
+        "num_draw": 1,
+        "x_rand_expected": 5.0,
+    }
+
+    return data
+
+
 def test_cdf_calculator(test_case_1):
     """
     Checking that the cdf is correctly calculated for given pdf and x array.
     """
     cdf_out = cc.cdf_calculator(test_case_1["x"], ip.pdf_initial_height)
     assert np.isclose(cdf_out, test_case_1["cdf_expected"]).all()
+
+
+def test_random_from_cdf(monkeypatch, test_case_2):
+    """
+    Checking that random numbers are correctly drawn from a pdf
+    """
+
+    def mock_cdf_rand(*args, **kwargs):
+        return 0.5
+
+    monkeypatch.setattr(np.random, "uniform", mock_cdf_rand)
+
+    x_rand_out = cc.random_from_cdf(
+        test_case_2["x"], test_case_2["cdf"], test_case_2["num_draw"]
+    )
+
+    assert np.abs(test_case_2["x_rand_expected"] - x_rand_out) < TOL
