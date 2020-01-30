d3fecbd391cf6b5ec36e01a92787b5e6b3f78069
MicheleRonchi <ronchi@ice.csic.es>
2020-01-30 14:19:50 +0100

    small change in the test_random_from_pdf

--- pypopsyn/simulator/tests/test_cdf_calculator.py
+++ pypopsyn/simulator/tests/test_cdf_calculator.py
@@ -54,7 +54,7 @@ def test_random_from_cdf(monkeypatch, test_case_2):
     assert np.abs(test_case_2["x_rand_expected"] - x_rand_out) < TOL
 
 
-def test_random_from_pdf(monkeypatch):
+def test_random_from_pdf(monkeypatch, test_case_2):
     """
     Checking that random numbers are correctly drawn from a pdf
     """
@@ -62,17 +62,13 @@ def test_random_from_pdf(monkeypatch):
     def pdf(x: float) -> float:
         return 1.0 / 10.0
 
-    x_grid = np.linspace(0.0, 10.0, 5)
-
-    num_draw = 1
-
-    x_rand_expected = 5.0
-
     def mock_cdf_rand(*args, **kwargs):
         return 0.5
 
     monkeypatch.setattr(np.random, "uniform", mock_cdf_rand)
 
-    x_rand_out = cc.random_from_pdf(x_grid, pdf, num_draw)
+    x_rand_out = cc.random_from_pdf(
+        test_case_2["x"], pdf, test_case_2["num_draw"]
+    )
 
-    assert np.abs(x_rand_expected - x_rand_out) < TOL
+    assert np.abs(test_case_2["x_rand_expected"] - x_rand_out) < TOL
