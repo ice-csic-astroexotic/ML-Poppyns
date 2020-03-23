d675d18c406dfb50eb512038c282877aac83523d
MicheleRonchi <ronchi@ice.csic.es>
2020-03-23 15:46:07 +0100

    Issue #11: simplifying the way we read the multirun folders as Albert suggested.

--- examples/generator/generating_dataset.py
+++ examples/generator/generating_dataset.py
@@ -1,7 +1,8 @@
 """ Generator for dataset.
 
     This module create a dataset of the simulated populations.
-    For every population simulated from the simulator `generate_evolve_population.py` with different initial parameters, a density map is created.
+    For every population simulated from the simulator `initialize_evolve_population.py`
+    with different initial parameters, a density map is created.
     This expects that a population has been generated using hydra multirun method.
     The simulated dataset is also saved in a .csv file where the density map .png
     files and the correponding parameters are saved.
@@ -30,96 +31,97 @@ log = logging.getLogger(__name__)
 
 
 def generate_dataset(args) -> None:
+    """
+        This method reads the simulated population files saved in a multirun/date/time
+        folder and generates a dataset of density map .png files for each population.
+        All the information about the dataset are stored in a datset.csv file
+        containing the density map files names and the set of parameter values for
+        each simulated population.
+    Args:
+        args:
+            date (str): Date in the form yyyy-mm-dd when the simulated population
+            files have been created.
+
+            time (str): Time in the form hh-mm-ss when the simulated population files
+            have been created.
+
+            dataset_name (str): Name of the dataset where the density maps will be saved
+    """
+
+    # create the dataset directory
     dataset_path = "examples/data/{}/{}/{}".format(
         args.date, args.time, args.dataset_name
     )
     pathlib.Path(dataset_path).mkdir(parents=True, exist_ok=True)
 
-    sample_index = 0
+    # initialize dictionaries that will contain the density map files names and the
+    # corresponding set of parameters values
     filename_dictionary = {}
     param_dictionary = {}
 
+    # Check if the parsed multirun directory exists as a precondition.
     root_path = pathlib.Path("multirun/{}/{}".format(args.date, args.time))
-    # Check if the directory exists as a precondition.
     if not root_path.exists():
         log.error("directory {} not found".format(root_path))
         sys.exit()
 
-    for root, dirs, files in os.walk(root_path):
-        # os.walk() returns the list of directories in arbitrary order, therefore
-        # sorting them is needed to give the correct association between .png
-        # density map filename and label
-        dirs.sort()
+    # number of samples in the parsed directory
+    sample_number = len(os.listdir(root_path))
 
-        for file_name in files:
-            # search for the population file
-            if file_name == args.file_name:
-                pop_path = pathlib.Path(os.path.join(root, args.file_name))
+    for s in range(sample_number):
 
-                if not pop_path.exists():
+        # Check if the simulated population file exists as a precondition.
+        pop_path = pathlib.Path(
+            "{}/{}/final_population.txt".format(root_path, s)
+        )
 
-                    log.error(
-                        "Population file not found in {}".format(pop_path)
-                    )
-                    sys.exit()
+        if not pop_path.exists():
+            log.error("Population file not found in {}".format(pop_path))
+            sys.exit()
 
-                # Check if the population files exists as a precondition.
-                if pop_path == pathlib.Path(
-                    os.path.join(root, "initial_population.txt")
-                ):
-                    log.warning(
-                        "Wrong filename: you inserted the initial population "
-                        "file instead of the evolved population file."
-                    )
-                    sys.exit()
+        # generate density map
+        df_pop = pd.read_csv(pop_path, skiprows=[1])
 
-                # generate density map
-                df_pop = pd.read_csv(pop_path, skiprows=[1])
+        density_map_filename = "{}/density_map_pop_{}.png".format(
+            dataset_path, s
+        )
 
-                density_map_filename = "{}/density_map_pop_{}.png".format(
-                    dataset_path, sample_index
-                )
+        dg.generate_density_map(
+            df_pop["x"],
+            (-20.0, 20.0),
+            df_pop["y"],
+            (-20.0, 20.0),
+            density_map_filename,
+            log_scale=False,
+        )
 
-                dg.generate_density_map(
-                    df_pop["x"],
-                    (-20.0, 20.0),
-                    df_pop["y"],
-                    (-20.0, 20.0),
-                    density_map_filename,
-                    log_scale=False,
-                )
+        log.info(
+            "density map .png generated for sample {} and saved in {}".format(
+                s, dataset_path
+            )
+        )
 
-                log.info(
-                    "density map .png generated for sample {} and saved in {}".format(
-                        sample_index, dataset_path
-                    )
-                )
+        # save filenames into a dictionary
+        filename_dictionary.setdefault("filename", []).append(
+            density_map_filename
+        )
 
-                # save filenames into a dictionary
-                filename_dictionary.setdefault("filename", []).append(
-                    density_map_filename
-                )
+        # Check if files containing labels exists as a precondition.
+        label_path = pathlib.Path(
+            "{}/{}/.hydra/config.yaml".format(root_path, s)
+        )
 
-                sample_index += 1
+        if not label_path.exists():
+            log.error(
+                "File containing labels not found in {}".format(label_path)
+            )
+            sys.exit()
 
-            # search for the label file
-            elif file_name == "config.yaml":
-                label_path = pathlib.Path(os.path.join(root, "config.yaml"))
-
-                # Check if files containing labels exists as a precondition.
-                if not label_path.exists():
-                    log.error(
-                        "File containing labels not found in {}".format(
-                            label_path
-                        )
-                    )
-                    sys.exit()
-
-                # save the parameters in a dictionary
-                with open(label_path) as file:
-                    dictionary = yaml.full_load(file)
-                    for key, val in dictionary.items():
-                        param_dictionary.setdefault(key, []).append(val)
+        # save the parameters value in a dictionary
+        with open(label_path) as file:
+            dictionary = yaml.full_load(file)
+            for key, val in dictionary.items():
+                param_dictionary.setdefault(key, []).append(val)
 
     # Merge the filename and parameters dictionaries in a single dictionary
     dataset_dictionary = {**filename_dictionary, **param_dictionary}
@@ -151,13 +153,6 @@ if __name__ == "__main__":
         default="12-00-00",
         help="Time in the form hh-mm-ss when the simulated population files have "
         "been created",
-    )
-    parser.add_argument(
-        "--file_name",
-        nargs="?",
-        type=str,
-        default="final_population.txt",
-        help="Simulated population file name",
     )
     parser.add_argument(
         "--dataset_name",
