fix: changing the absolute tolerance value in the np.isclose asserts in the tests to a small negative value, to actually pick up on wrong values in the test cases.

pypopsyn/simulator/tests/test_galactic_model.py
@@ -104,7 +104,7 @@ def test_dh_potential_FK06(test_case_1):
     pot_dh_out = gmFK06.dh_potential(test_case_1["r"], test_case_1["z"])
 
     assert np.isclose(
-        pot_dh_out, test_case_1["pot_dh_expected"], rtol=TOL, atol=1.0e9,
+        pot_dh_out, test_case_1["pot_dh_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -115,7 +115,7 @@ def test_b_potential_FK06(test_case_1):
     pot_b_out = gmFK06.b_potential(test_case_1["r"])
 
     assert np.isclose(
-        pot_b_out, test_case_1["pot_b_expected"], rtol=TOL, atol=1.0e9,
+        pot_b_out, test_case_1["pot_b_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -126,7 +126,7 @@ def test_n_potential_FK06(test_case_1):
     pot_n_out = gmFK06.n_potential(test_case_1["r"])
 
     assert np.isclose(
-        pot_n_out, test_case_1["pot_n_expected"], rtol=TOL, atol=1.0e9,
+        pot_n_out, test_case_1["pot_n_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -137,7 +137,7 @@ def test_MW_potential_FK06(test_case_1):
     pot_MW_out = gmFK06.MW_potential(test_case_1["r"], test_case_1["z"])
 
     assert np.isclose(
-        pot_MW_out, test_case_1["pot_MW_expected"], rtol=TOL, atol=1.0e10,
+        pot_MW_out, test_case_1["pot_MW_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -153,7 +153,7 @@ def test_tot_energy_FK06(test_case_1):
         tot_energy_out,
         test_case_1["tot_energy_expected"],
         rtol=TOL,
-        atol=1.0e10,
+        atol=1.0e-30,
     )
 
 
@@ -244,7 +244,7 @@ def test_d_potential_M19(test_case_2):
     pot_d_out = gmM19.d_potential(test_case_2["r"], test_case_2["z"])
 
     assert np.isclose(
-        pot_d_out, test_case_2["pot_d_expected"], rtol=TOL, atol=1.0e9,
+        pot_d_out, test_case_2["pot_d_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -255,7 +255,7 @@ def test_b_potential_M19(test_case_2):
     pot_b_out = gmM19.b_potential(test_case_2["r"])
 
     assert np.isclose(
-        pot_b_out, test_case_2["pot_b_expected"], rtol=TOL, atol=1.0e9,
+        pot_b_out, test_case_2["pot_b_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -266,7 +266,7 @@ def test_n_potential_M19(test_case_2):
     pot_n_out = gmM19.n_potential(test_case_2["r"])
 
     assert np.isclose(
-        pot_n_out, test_case_2["pot_n_expected"], rtol=TOL, atol=1.0e9,
+        pot_n_out, test_case_2["pot_n_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -277,7 +277,7 @@ def test_h_potential_M19(test_case_2):
     pot_h_out = gmM19.h_potential(test_case_2["r"])
 
     assert np.isclose(
-        pot_h_out, test_case_2["pot_h_expected"], rtol=TOL, atol=1.0e9,
+        pot_h_out, test_case_2["pot_h_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -288,7 +288,7 @@ def test_MW_potential_M19(test_case_2):
     pot_MW_out = gmM19.MW_potential(test_case_2["r"], test_case_2["z"])
 
     assert np.isclose(
-        pot_MW_out, test_case_2["pot_MW_expected"], rtol=TOL, atol=1.0e10,
+        pot_MW_out, test_case_2["pot_MW_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -304,7 +304,7 @@ def test_tot_energy_M19(test_case_2):
         tot_energy_out,
         test_case_2["tot_energy_expected"],
         rtol=TOL,
-        atol=1.0e10,
+        atol=1.0e-30,
     )
 
 

pypopsyn/simulator/tests/test_magnetic_field_evolution.py
@@ -39,9 +39,9 @@ def test_case_1():
         "sigma": 1e24,
         "L": 1e5,
         "n_e": 1e36,
-        "B": np.array([1e12, 1e14]),
-        "tau_ohm_expected": 4433654.543524,
-        "tau_Hall_expected": np.array([63843047.417426, 638430.474174]),
+        "B": 1e12,
+        "tau_ohm_expected": 4.43365e6,
+        "tau_Hall_expected": 63.8430471e6,
     }
 
     return data
@@ -50,11 +50,10 @@ def test_case_1():
 @pytest.fixture()
 def test_case_2():
     data = {
-        "B_initial": np.array([1e12, 1e13, 1e14]),
-        "B": np.array([1e11, 5e12, 1e10]),
-        "B_deriv_expected": np.array(
-            [-0.720173e-05, -4817.742130e-05, -7.157033e-05]
-        ),
+        "B_initial": 1e13,
+        "t": 10.0,
+        "B": 5e12,
+        "B_deriv_expected": -4.817742e-02,
     }
 
     return data
@@ -67,7 +66,7 @@ def test_timescale_ohmic(test_case_1):
     tau_ohm_out = mfe.timescale_ohmic(test_case_1["L"], test_case_1["sigma"])
 
     assert np.isclose(
-        tau_ohm_out, test_case_1["tau_ohm_expected"], rtol=TOL, atol=1.0e9,
+        tau_ohm_out, test_case_1["tau_ohm_expected"], rtol=TOL, atol=1.0e-30,
     )
 
 
@@ -80,18 +79,18 @@ def test_timescale_Hall(test_case_1):
     )
 
     assert np.isclose(
-        tau_Hall_out, test_case_1["tau_Hall_expected"], rtol=TOL, atol=1.0e9,
-    ).all()
+        tau_Hall_out, test_case_1["tau_Hall_expected"], rtol=TOL, atol=1.0e-30,
+    )
 
 
-def test_magnetic_field_derivative(test_case_2):
+def test_field_derivative(test_case_2):
     """
-    Verifying that the magnetic field derivatives are evaluated correctly.
+    Verifying that the magnetic field derivative is evaluated correctly.
     """
     B_deriv_out = mfe.field_derivative(
-        test_case_2["B"], test_case_2["B_initial"]
+        test_case_2["t"], test_case_2["B"], test_case_2["B_initial"]
     )
 
     assert np.isclose(
-        B_deriv_out, test_case_2["B_deriv_expected"], rtol=TOL, atol=1.0e9,
-    ).all()
+        B_deriv_out, test_case_2["B_deriv_expected"], rtol=TOL, atol=1.0e-30,
+    )

pypopsyn/simulator/tests/test_misalignment_angle_evolution.py
@@ -38,16 +38,16 @@ def test_case_1():
     data = {
         "B": np.array([1e12, 1e13, 1e14, 1e15]),
         "chi": np.array([0, np.pi / 3, 1.4 * np.pi, 2.6 * np.pi]),
-        "P": np.array([1.0e-3, 1.0e-1, 1, 5]),
+        "P": np.array([1.0e-2, 1.0e-1, 1.0, 5.0]),
         "chi_deriv_expected": np.array(
-            [0.0, -2.073438e-12, -1.407275e-14, 5.629101e-14]
+            [0.0, -2.073438e-12, -1.407275e-12, 5.629101e-12]
         ),
     }
 
     return data
 
 
-def test_period_derivative(test_case_1):
+def test_misalignment_angle_derivative(test_case_1):
     """
     Verifying that the misalignment angle derivatives for a pulsar sample are evaluated correctly.
     """
@@ -56,5 +56,8 @@ def test_period_derivative(test_case_1):
     )
 
     assert np.isclose(
-        chi_deriv_out, test_case_1["chi_deriv_expected"], rtol=TOL, atol=1.0e9,
+        chi_deriv_out,
+        test_case_1["chi_deriv_expected"],
+        rtol=TOL,
+        atol=1.0e-30,
     ).all()

pypopsyn/simulator/tests/test_period_evolution.py
@@ -56,5 +56,5 @@ def test_period_derivative(test_case_1):
     )
 
     assert np.isclose(
-        P_deriv_out, test_case_1["P_deriv_expected"], rtol=TOL, atol=1.0e9,
+        P_deriv_out, test_case_1["P_deriv_expected"], rtol=TOL, atol=1e-30,
     ).all()