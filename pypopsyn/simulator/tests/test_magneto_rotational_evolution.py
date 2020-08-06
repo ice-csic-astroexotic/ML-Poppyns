ed66d3a2fa9ec1916ea8a9c57f788120bb25ceda
Vanessa Graber <graber@ice.csic.es>
2020-08-06 16:35:57 +0200

    Adding a module to perform the combined evolution of the pulsar magnetic field, misalignment angle and rotation period, with a corresponding test.

---
+++ pypopsyn/simulator/tests/test_magneto_rotational_evolution.py
@@ -0,0 +1,101 @@
+"""
+Tests for the magneto_rotational_physics/magneto_rotational_evolution module.
+
+    Authors:
+
+        Vanessa Graber (graber @ ice.csic.es)
+
+MIT License
+
+Copyright (c) MAGNESIA (ICE-CSIC) 2020
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import numpy as np
+import pytest
+
+import pypopsyn.simulator.magneto_rotational_physics.magneto_rotational_evolution as mre
+
+TOL = 1e-5
+
+
+@pytest.fixture()
+def test_case_1():
+    data = {
+        "B_initial": np.array([1e10, 1e12]),
+        "chi_initial": np.array([0.0, np.pi / 3]),
+        "P_initial": np.array([1e-2, 1.0]),
+        "time_step": 1e3,
+        "NS_number": 2,
+        "t_age": np.array([1e3, 1e3]),
+        "B_final_expected": np.array([9.997743e9, 9.997588e11]),
+        "chi_final_expected": np.array([0.0, 1.047198]),
+        "P_final_expected": np.array(
+            [0.010000000000004788, 1.000000000000838]
+        ),
+    }
+
+    return data
+
+
+def test_combined_derivatives(test_case_1):
+    """
+    Testing that the output of the combined derivatives has the correct shape.
+    """
+    y = np.array(
+        [
+            test_case_1["B_initial"][0],
+            test_case_1["chi_initial"][0],
+            test_case_1["P_initial"][0],
+        ]
+    )
+
+    dy = mre.combined_derivatives(0, y, test_case_1["B_initial"][0])
+    assert len(dy) == 3
+
+
+def test_magneto_rotational_evolution(test_case_1):
+    """
+    Verifying (approximately) that the magnetic field, misalignment angle and period are
+    correctly evolved in time. To do so, we use a simple finite differencing scheme, i.e.,
+    x_initial + x_derivative * time_step, to evaluate the first time step, only, and compare
+    it to the output of solve_ivp for two object whose ages correspond exactly to the length
+    of the first time step. Note that
+    """
+    B_final_out, chi_final_out, P_final_out = mre.magneto_rotational_evolution(
+        test_case_1["B_initial"],
+        test_case_1["chi_initial"],
+        test_case_1["P_initial"],
+        test_case_1["NS_number"],
+        test_case_1["t_age"],
+        test_case_1["time_step"],
+    )
+    assert np.isclose(
+        B_final_out, test_case_1["B_final_expected"], rtol=TOL, atol=1.0e-30
+    ).all()
+
+    assert np.isclose(
+        chi_final_out,
+        test_case_1["chi_final_expected"],
+        rtol=TOL,
+        atol=1.0e-30,
+    ).all()
+
+    # The relative changes in the period are very small, so we adjust rtol to a smaller value.
+    assert np.isclose(
+        P_final_out, test_case_1["P_final_expected"], rtol=1e-15, atol=1.0e-30
+    ).all()
