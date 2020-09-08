f7b225a1957bbf127e1464112e0b2c083d8b2178
ronchi@ice.csic.es <mronchi@gpu01.pic.es>
2020-09-08 09:48:37 +0200

    Correcting some errors in the implementation of the convolution network architecture and on the forward pass and adding the related in line documentation.

--- pypopsyn/learning/models/model_conv.py
+++ pypopsyn/learning/models/model_conv.py
@@ -23,7 +23,9 @@ class ModelConv(ModelBase):
 
     def __init__(self, input_shape: np.array, num_parameters: int = 1) -> None:
 
-        """ CNN Model Initialization.
+        """
+        CNN Model Initialization.
+        This CNN automatically adapts to the shape of the initial input features.
 
         Args:
             input_shape: Shape of the input batch (C x H x W).
@@ -34,25 +36,37 @@ class ModelConv(ModelBase):
         super().__init__()
         self.conv1 = nn.Conv2d(input_shape[0], 4, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        # TODO: This will need to adapt to different image sizes.
-        x = torch.randn(input_shape[0], input_shape[1], input_shape[2])
+        
+        # Create a mock input with the same shape of the real input drawing values from a normal distribution and pass it through the convolution layers in order to save the shape of the input features after the convolution layer and automatically initialize the linear layers with the right shape. 
+        x = torch.randn(input_shape[0], input_shape[1], input_shape[2]).view(-1,input_shape[0], input_shape[1], input_shape[2])
         self._to_linear = None
         self.convs(x)
 
         self.fc1 = nn.Linear(self._to_linear, num_parameters)
 
     def convs(self, x):
+        """
+        Convolution and pooling layers farward pass.
+
+        Args:
+            x: Input tensor for the convolution layers.
+  
+        Returns:
+            Output tensor of the convolution and pooling layers.
+        """
 
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
 
+        # If the dimension of the flattened input features to the linear layers has not been saved yet, save it.
         if self._to_linear is None:
             self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+
         return x
 
     def forward(self, x):
 
-        """ Forward pass.
+        """ 
+	Forward pass.
 
         Args:
             x: Input tensor for the network.
@@ -62,9 +76,8 @@ class ModelConv(ModelBase):
 
         """
 
-        self.convs(x)
-        # TODO: This will need to adapt to different image sizes.
-        x = x.view(-1, self._to_linear)
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)        
         x = F.relu(self.fc1(x))
 
         return x
