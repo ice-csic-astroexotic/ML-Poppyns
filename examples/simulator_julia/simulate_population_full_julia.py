6f17953fe18a95d8f59af8b47fd4c591a2d4fb1a
celsa <pardoaraujocelsa@gmail.com>
2022-08-04 14:22:03 +0200

    Changing the structure of the example folder to have one folder called simulator_julia with the julia file. Fixing a bug in the simulate_population_full_julia.py script that was using json when it has to use orjson.

--- examples/simulator_julia/simulate_population_full_julia.py
+++ examples/simulator_julia/simulate_population_full_julia.py
@@ -44,6 +44,7 @@ import sys
 import time
 
 import numpy as np
+import orjson
 import pandas as pd
 from julia import Main
 
@@ -410,9 +411,16 @@ def simulate_population(args) -> None:
                 dyn_evolution_dump_path = pathlib.Path().joinpath(
                     output_path, "dyn_evolution.json"
                 )
-                with open(dyn_evolution_dump_path, "w") as f:
-                    json.dump(dyn_evol_dict, f, indent=4, sort_keys=True)
 
+                with open(dyn_evolution_dump_path, "wb") as f:
+                    f.write(
+                        orjson.dumps(
+                            dict(dyn_evol_dict),
+                            option=orjson.OPT_SERIALIZE_NUMPY
+                            | orjson.OPT_NON_STR_KEYS
+                            | orjson.OPT_SORT_KEYS,
+                        )
+                    )
             timer.checkpoint("[Dynamical evolution]")
 
             # Compute the magnitude of the initial velocity vector for each star.
