a12049b9e5bb7ab6c7e750165255b8729657ec4a
MicheleRonchi <ronchi@ice.csic.es>
2022-09-23 11:07:02 +0200

    Updating the parameter sweeper to include the log-normal period distribution and to make it flexible to run both in the server and on the laptop.

--- pypopsyn/simulator/configuration.py
+++ pypopsyn/simulator/configuration.py
@@ -54,10 +54,12 @@ cfg["server_run"] = False
 
 if cfg["server_run"]:
     cfg[
-        "path_server"
+        "path_server_software"
     ] = "/data/magnesia/software/MAGNESIA_population_synthesis/"
+    cfg["path_server_output"] = "/data/magnesia/common/"
 else:
-    cfg["path_server"] = ""
+    cfg["path_server_software"] = ""
+    cfg["path_server_output"] = ""
 
 # ===================== INITIAL POPULATION CLASS PARAMETERS ========================
 
