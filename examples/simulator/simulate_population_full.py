17a777e926d978798c4bee7c95c5a91f4a0f060f
Vanessa Graber <graber@ice.csic.es>
2023-01-11 10:06:04 +0100

    Add a comment line for variabler path_server_software.

--- examples/simulator/simulate_population_full.py
+++ examples/simulator/simulate_population_full.py
@@ -115,11 +115,13 @@ def simulate_population(args) -> None:
     with open(config_dump_path, "w") as f:
         json.dump(cfg, f, indent=4, sort_keys=True)
 
+    # Loading the sever path, in case simulations are run at the PIC.
+    path_server_software = cfg["path_server_software"]
+
     # Initialize components of the simulator that need it.
     gm.initialize_galactic_model()
     sm.initialize_spiral_model()
 
-    path_server_software = cfg["path_server_software"]
     # Initialize the surveys.
     PMPS_par_path = pathlib.Path().joinpath(
         path_server_software,
