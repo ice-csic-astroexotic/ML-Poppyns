f071b0778fb5df3d3af6f01fc68fe7558798bfec
celsa <pardoaraujocelsa@gmail.com>
2023-01-27 16:46:57 +0100

    Changing the name of the environment from pop_syn_test to pop_syn

--- scripts/generate_htcondor_submit.py
+++ scripts/generate_htcondor_submit.py
@@ -120,9 +120,7 @@ def generate_wrapper(
         f.write(
             "source /data/astro/software/centos7/conda/mambaforge_4.14.0/etc/profile.d/conda.sh\n"
         )
-        f.write(
-            "conda activate /data/magnesia/scratch/conda/env/pop_syn_test\n"
-        )
+        f.write("conda activate /data/magnesia/scratch/conda/env/pop_syn\n")
         f.write(
             "# We copy the pypopsyn module in the working node to avoid problems with the path while running the simulations in the server.\n"
         )
