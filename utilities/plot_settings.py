d66255f0dee737bcab2c43c5544b72e4253844b8
celsa <pardoaraujocelsa@gmail.com>
2023-05-31 17:57:27 +0200

    Changes to make the labels size bigger and change the label of the luminosity for Edot.

--- utilities/plot_settings.py
+++ utilities/plot_settings.py
@@ -40,10 +40,10 @@ rcParams["figure.autolayout"] = True
 rcParams["axes.linewidth"] = "1.7"
 rcParams["axes.labelpad"] = "15.0"
 rcParams["axes.titlepad"] = "15.0"
-rcParams["axes.labelsize"] = "30.0"
+rcParams["axes.labelsize"] = "35.0"
 rcParams["axes.titlesize"] = "30.0"
 
-rcParams["legend.fontsize"] = "30.0"
+rcParams["legend.fontsize"] = "25.0"
 
 rcParams["xtick.direction"] = "in"
 rcParams["xtick.top"] = True
@@ -53,7 +53,7 @@ rcParams["xtick.major.size"] = "10.0"
 rcParams["xtick.major.width"] = "1.7"
 rcParams["xtick.minor.size"] = "5.0"
 rcParams["xtick.minor.width"] = "1.7"
-rcParams["xtick.labelsize"] = "30"
+rcParams["xtick.labelsize"] = "35"
 
 rcParams["ytick.direction"] = "in"
 rcParams["ytick.right"] = True
@@ -63,4 +63,4 @@ rcParams["ytick.major.size"] = "10.0"
 rcParams["ytick.major.width"] = "1.7"
 rcParams["ytick.minor.size"] = "5.0"
 rcParams["ytick.minor.width"] = "1.7"
-rcParams["ytick.labelsize"] = "30"
+rcParams["ytick.labelsize"] = "35"
