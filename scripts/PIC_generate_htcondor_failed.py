cb71e1bec9a95b7437a113202125f776d06556ad
MicheleRonchi <ronchi@ice.csic.es>
2023-07-12 14:54:22 +0200

    Correcting some docstrings in scripts/PIC_generate_htcondor_failed.py.

--- scripts/PIC_generate_htcondor_failed.py
+++ scripts/PIC_generate_htcondor_failed.py
@@ -41,12 +41,16 @@ import pandas as pd
 
 def generate_htcondor_failed(args):
     """
-    Copying the successfully relaunched output back to the original folders.
+    Create the submit file and wrapper file necessary to relaunch the failed simulations.
 
     Args:
         args:
-            simulation_dir (pathlib.Path): Output directory where the simulation outputs are.
-            failed_simulation_dir (pathlib.Path): Output directory where the failed simulation outputs are.
+            output_dir_simulation (pathlib.Path): Output directory where the simulation outputs are.
+            number_sim_job (int): Number of simulations per job were chosen when running the original set of
+                simulations with HTCondor.
+            dyn_data (pathlib.Path): Path to the file where the dynamically evolved population database is stored.
+            type_simulation (str): Type of simulation that we want to run in the PIC with HTCondor.
+                Choose between dyn or magrot.
 
     Returns:
         Nothing.
