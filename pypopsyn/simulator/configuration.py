d1d6c407c937bf06211bb44f873501f5cf40ecf6
Vanessa Graber <graber@ice.csic.es>
2023-11-28 13:10:18 +0100

    Modifying the docstring for the radio luminosity normalisation parameter to correct the units.

--- pypopsyn/simulator/configuration.py
+++ pypopsyn/simulator/configuration.py
@@ -215,8 +215,8 @@ cfg["B_millisec_sigma"] = 0.5
 # (see Johnston et al. 2020).
 cfg["r_em"]: float = 3.0e7
 
-# Mean and standard deviation for the log-normally distributed radio luminosity in [erg s^(-1) Hz^(-1)].
-# Parameters were adjusted to match observed data.
+# Mean and standard deviation for the log-normally distributed radio luminosity normalization factor
+# Given in [erg s^(3 * epsilon_L - 1) ]. Parameters were adjusted to match observed data.
 cfg["L_radio_log10_mean"]: float = 35.5
 cfg["L_radio_log10_sigma"]: float = 0.8
 cfg["epsilon_L"]: float = 0.5
