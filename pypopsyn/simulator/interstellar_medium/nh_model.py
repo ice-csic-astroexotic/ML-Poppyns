ffcdd31d80f1398f120b5a4d93f131b98dad62e9
MicheleRonchi <ronchi@ice.csic.es>
2024-03-25 17:08:03 +0100

    Adding a module to estimate the N_H for x-ray absorption along with the relative test, and a script that download a file.

---
+++ pypopsyn/simulator/interstellar_medium/nh_model.py
@@ -0,0 +1,143 @@
+"""
+Model for computing the hydrogen column density for x-ray absorption.
+
+For computing the N_H we provide two options:
+1) Using the map and routines from the 3D NH-tool by Doroshenko 2024, available for download at
+https://zenodo.org/records/10779060.
+2) Using the relation between NH and DM found by He, Ng and Kaspi 2013.
+
+Authors:
+
+        Michele Ronchi (ronchi@ice.csic.es)
+
+MIT License
+
+Copyright (c) MAGNESIA (ICE-CSIC) 2024
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import logging
+import os
+import pathlib
+import sys
+
+import astropy.coordinates as c
+import astropy.units as u
+import healpy as hp
+import numpy as np
+
+import scripts.download_file as df
+from pypopsyn.simulator.configuration import cfg
+
+log = logging.getLogger(__name__)
+
+
+def compute_NH(RA: np.ndarray, DEC: np.ndarray, d: np.ndarray) -> np.ndarray:
+    """
+    Given an array of positions in equatorial coordinates and distances, return the corresponding line of sight N_H
+    using Wilms et al. 2000 abundances in given direction.
+    For computing the N_H we used the map and routines from Doroshenko 2024, available for download at
+    https://zenodo.org/records/10779060.
+    Args:
+        RA (np.ndarray): array of right ascension coordinates in deg.
+        DEC (np.ndarray): array of declination coordinates in deg.
+        d (np.ndarray): array of distances in kpc.
+    Return:
+        N_H (np.ndarray): array of hydrogen column densities in cm^(-2).
+    """
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    # Import the extinction maps from Doroshenko 2024.
+    extinction_map_path = pathlib.Path().joinpath(
+        cfg["path_server_software"],
+        "pypopsyn/simulator/interstellar_medium/ebv_map.npz",
+    )
+    if not os.path.exists(extinction_map_path):
+        download_url = (
+            "https://zenodo.org/records/10779060/files/ebv_fin.npz?download=1"
+        )
+        destination_path = extinction_map_path
+        log.info(f"Downloading the extinction map from: {download_url}")
+        df.download_file(download_url, destination_path)
+
+    maps = np.load(extinction_map_path)["maps"].T
+    # Import the distance bins.
+    dbins = np.load(extinction_map_path)["radius"]
+    # Import the calibration parameters.
+    (
+        Rv,
+        calib_avks_mean,
+        calib_avks_std,
+        calib_nhag89_mean,
+        calib_nhag89_std,
+        calib_nhw00_mean,
+        calib_nhw00_std,
+    ) = [
+        np.load(extinction_map_path)["%s" % x]
+        for x in [
+            "Rv",
+            "calib_avks_mean",
+            "calib_avks_std",
+            "calib_nhag89_mean",
+            "calib_nhag89_std",
+            "calib_nhw00_mean",
+            "calib_nhw00_std",
+        ]
+    ]
+
+    # Create an astropy.coordinates.SkyCoord object.
+    pos = c.SkyCoord(RA * u.deg, DEC * u.deg, distance=d * u.kpc, frame="fk5")
+    # Transform to Galactic coordinates and the distance in pc.
+    gpos = pos.transform_to(c.Galactic)
+    dist = np.array(gpos.distance.pc)
+
+    # Find the pixel of the map corresponding to the given coordinates.
+    gpix = np.array(hp.ang2pix(256, gpos.l.deg, gpos.b.deg, lonlat=True))
+
+    # If distance exceed 25 kpc set it to the maximum distance in the map, i.e. 25 kpc.
+    dist[dist > 25000] = np.array([dbins[-1]])
+
+    # Extract the extintion in the visual band.
+    ebv = maps[gpix, :]
+    # Use Wilms et al. 2000 abundances by default to calculate N_H from extinction.
+    nh_conv_fac = calib_nhw00_mean[0]
+    nh = ebv * nh_conv_fac
+
+    # Extract the indices corresponding to the given distances from the map.
+    idx = np.minimum(dbins.searchsorted(dist), len(dbins) - 1)
+
+    # Extract the N_H for every star.
+    N_H = np.array(nh[np.arange(nh.shape[0]), idx] * 10 ** (21), dtype=float)
+
+    return N_H
+
+
+def compute_NH_from_DM(DM: np.ndarray) -> np.ndarray:
+    """
+    Given an array of dispersion measures DM, estimate the corresponding line of sight N_H
+    using the relation found by He, Ng and Kaspi 2013.
+
+    Args:
+        DM (np.ndarray): array of dispersion measures in pc cm^-3.
+
+    Return:
+        N_H (np.ndarray): array of hydrogen column densities in cm^(-2).
+    """
+    N_H = np.array([0.3 * DM * 10 ** (20)], dtype=float)
+
+    return N_H
---
+++ scripts/download_file.py
@@ -0,0 +1,58 @@
+"""
+This script download a file from a given url into the specified directory path.
+
+Authors:
+
+    Michele Ronchi (ronchi@ice.csic.es)
+
+Copyright (c) MAGNESIA (ICE-CSIC) 2024
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import logging
+import sys
+
+import requests
+
+log = logging.getLogger(__name__)
+
+
+def download_file(download_url: str, destination_path: str) -> None:
+    """
+    Download a file from a given url into the specified directory path.
+
+    Args:
+        download_url (str): download url for the file.
+        destination_path (str): path where to save the downloaded file, the file name has to be included.
+    """
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    try:
+        # Send an HTTP GET request to the URL to download the file.
+        with requests.get(download_url, stream=True) as response:
+            # Check if the request was successful (status code 200).
+            if response.status_code == 200:
+                # Open a file handle to write the response content to.
+                with open(destination_path, "wb") as f:
+                    # Iterate over the response content in chunks and write each chunk to the file.
+                    for chunk in response.iter_content(chunk_size=8192):
+                        f.write(chunk)
+                log.info("Download completed successfully.")
+            else:
+                log.info(
+                    f"Failed to download the file. Status code: {response.status_code}"
+                )
+    except Exception as e:
+        log.error(f"An error occurred: {str(e)}")
