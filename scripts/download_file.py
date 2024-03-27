0f8f795a520d57be9213dad1fa223bc04f3c59ec
Vanessa Graber <graber@ice.csic.es>
2024-03-27 09:43:40 +0000

    Modifying a few inconsistencies in docstrings.

--- scripts/download_file.py
+++ scripts/download_file.py
@@ -1,17 +1,19 @@
 """
-This script download a file from a given url into the specified directory path.
+This script downloads a file from a given URL into the specified directory path.
 
 Authors:
 
     Michele Ronchi (ronchi@ice.csic.es)
 
 Copyright (c) MAGNESIA (ICE-CSIC) 2024
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,11 +33,11 @@ log = logging.getLogger(__name__)
 
 def download_file(download_url: str, destination_path: str) -> None:
     """
-    Download a file from a given url into the specified directory path.
+    Download a file from a given URL into the specified directory path.
 
     Args:
-        download_url (str): download url for the file.
-        destination_path (str): path where to save the downloaded file, the file name has to be included.
+        download_url (str): download URL for the file.
+        destination_path (str): path where to save the downloaded file; the file name has to be included.
     """
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
