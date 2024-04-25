30f2d4f9a8a8c38002f5ee8b1a67a941912bc204
celsa <pardoaraujocelsa@gmail.com>
2024-04-25 10:10:43 +0200

    First draft for using dask with the training tsnpe.

---
+++ scripts/simulation_helper_dask.py
@@ -0,0 +1,192 @@
+"""
+    Simulator helper script.
+
+    This script allows us to run various simulator scripts in a multithreaded manner. Unlike the `simulation_helper.py`
+    script, which sample parameters randomly or on a grid, this script follows a prior distribution for parameter
+    sampling. Note that this script can be run only when using a prior distribution from the sbi package that has the
+    .sample() method available.
+
+    The number of values to be drawn for each parameter is specified by the argument --sampling_size.
+
+    Each parameter combination will spawn a new process that enters a multithreaded pool for later execution,
+    allowing the asynchronous simulation of many populations in parallel with a defined maximum number of threads.
+
+    NOTE: if an error occurs in one of the simulations, the script will not stop until all the processes have been
+    terminated. The error will be only shown in the terminal in this case.
+
+    Running the code:
+
+        python simulator_helper_sbi.py --h
+        To obtain help about all the arguments that can be used.
+
+    Authors:
+
+        Celsa Pardo Araujo (pardo@ice.csic.es)
+        Michele Ronchi (ronchi@ice.csic.es)
+
+    Copyright (c) MAGNESIA (ICE-CSIC) 2020
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
+import json
+import logging
+import pathlib
+import subprocess
+
+import dask
+import torch
+from sbi.inference.posteriors.direct_posterior import DirectPosterior
+
+from pypopsyn.learning.loaders.loader_multichannel_array_stat import (
+    DatasetMultichannelArray,
+)
+from pypopsyn.simulator.configuration import cfg
+
+log = logging.getLogger(__name__)
+
+
+def run_simulation(command: str) -> None:
+    """
+    Run simulation command.
+
+    Args:
+        command (str): Full command to execute the simulation.
+
+    Returns:
+        None
+    """
+    log.info(f"Launching simulation: {command}")
+
+    try:
+        # Execute the simulation command
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        # Log any errors raised by the subprocess
+        log.error(f"Error executing command: {command}")
+        log.error(f"Command output: {e.output.decode('utf-8')}")
+        raise  # Reraise the exception to propagate the error
+
+    log.info("Simulation finished")
+
+
+def simulator(
+    args_dict: dict,
+    prior: DirectPosterior,
+    dataset: DatasetMultichannelArray,
+) -> None:
+
+    """
+    Execute simulations based on the provided prior distribution.
+
+    Args:
+        args_dict (Dictionary): Dictionary with the arguments.
+        prior (DirectPosterior): Prior distribution.
+        dataset (DatasetMultichannelArray):  Stores statistics and scaling information used in the prior distribution.
+
+    Returns:
+        None
+    """
+    # Create a list to hold delayed computations for each simulation
+    delayed_simulations = []
+
+    # Parse arguments provided to the simulation helper script.
+    log.info("Parsing arguments...")
+
+    # Extracting the names of the parameters.
+    var_names = dataset.target_names
+
+    # Save the statistics for the filtered labels.
+    par_max = torch.tensor(dataset.target_max)
+    par_min = torch.tensor(dataset.target_min)
+    par_std = torch.tensor(dataset.target_std)
+    par_mean = torch.tensor(dataset.target_mean)
+
+    # Create a generator of the random sets of parameters using the prior distribution.
+    parameter_sets_gen_tensor = prior.sample((args_dict["sampling_size"],))
+
+    # If the parameters were normalized or standardized, rescale quantities to their physical ranges.
+    if dataset.normalize:
+        parameter_sets_gen_tensor = (
+            parameter_sets_gen_tensor * (par_max - par_min) + par_min
+        )
+
+    elif dataset.standardize:
+        parameter_sets_gen_tensor = (
+            parameter_sets_gen_tensor * par_std + par_mean
+        )
+
+    parameter_sets_gen = [
+        tuple(subtensor.tolist()) for subtensor in parameter_sets_gen_tensor
+    ]
+
+    # Set the simulation type and the path to the dynamical database if required.
+    simulator_type = args_dict["simulator_type"]
+    dyn_data_path = ""
+    if simulator_type == "simulate_population_magrot_det":
+        dyn_data_path = args_dict["dyn_data"]
+
+    # Queue each set of parameters as a different simulation in the pool.
+    log.info("Queuing simulations...")
+
+    simulation_number: int = 0
+    for s in parameter_sets_gen:
+        log.info("Queuing simulation: ")
+        log.info(s)
+
+        # Generate output folder for the simulation.
+        # Note that the numbering of the folders is limited to 6 digits here,
+        # i.e., we can only generate simulations below 10 million.
+        simulation_output_path = pathlib.Path().joinpath(
+            args_dict["output_dir"], f"{simulation_number:06}"
+        )
+        simulation_output_path.mkdir(parents=True, exist_ok=True)
+
+        # Save the set of parameter values into a JSON override file and write it to the folder for a given simulation.
+        simulation_override_json = {}
+        for i in range(len(s)):
+            simulation_override_json[var_names[i]] = s[i]
+
+        simulation_override_json_path = pathlib.Path().joinpath(
+            simulation_output_path, "override.json"
+        )
+
+        with open(simulation_override_json_path, "w") as f:
+            json.dump(simulation_override_json, f, indent=4, sort_keys=True)
+
+        # Generate a list for the command (cmd), including the Python interpreter, the script path specified with
+        # 'simulator_type', and the path for the JSON override.
+        server_path = cfg["path_to_software"]
+        cmd: str = (
+            f"python {server_path}examples/simulator/{simulator_type}.py"
+        )
+        cmd += f" --output_dir {simulation_output_path}"
+        cmd += f" --parameter_override {simulation_override_json_path}"
+        if simulator_type == "simulate_population_magrot_det":
+            cmd += f" --dyn_data {dyn_data_path}"
+
+        # Create delayed computation for each simulation
+        delayed_simulation = run_simulation(cmd)
+        delayed_simulations.append(delayed_simulation)
+
+        simulation_number += 1
+
+    log.info("")
+    log.info("***************************************************************")
+    log.info("Launching simulations")
+    log.info("***************************************************************")
+
+    # Compute the delayed computations
+    dask.compute(delayed_simulations)
