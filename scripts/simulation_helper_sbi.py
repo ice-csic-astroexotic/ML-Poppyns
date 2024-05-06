ebf22357cf8d9d4e2ea8b9ab17aa851cbb2b4c31
celsa <pardoaraujocelsa@gmail.com>
2024-05-06 12:33:31 +0200

    Removing the req from the creation of the cluster since it is not needed.

--- scripts/simulation_helper_sbi.py
+++ scripts/simulation_helper_sbi.py
@@ -88,13 +88,10 @@ def initialize_dask_cluster(
     hctondor_output_folder = f"{config.save_dir}/htcondor_output"
     pathlib.Path(hctondor_output_folder).mkdir(parents=True, exist_ok=True)
     logger.info(
-        f"Saving the stdout and stderr of the terminal of each worker in {hctondor_output_folder}..."
+        f"Saving the stdout and stderr of the terminal of each worker in {hctondor_output_folder}."
     )
     # Creating the cluster with dask for HTCondor.
-    # The following requirements are specific for the computing resources at the PIC.
-    req = '(CPU_MODEL =!= "Intel(R) Xeon(R) CPU E5-2680 v4 @ 2.40GHz") && (CPU_MODEL =!= "AMD EPYC 7452 32-Core Processor")'
     extra = {
-        "requirements": req,
         "getenv": "True",
         "output": f"{hctondor_output_folder}/$(ClusterId)_$(ProcId)-out.txt",
         "error": f"{hctondor_output_folder}/$(ClusterId)_$(ProcId)-out.txt",
