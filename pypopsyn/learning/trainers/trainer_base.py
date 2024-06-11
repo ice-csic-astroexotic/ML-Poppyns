161f8d635aa471745ac59579652c33150e012e74
MicheleRonchi <ronchi@ice.csic.es>
2024-06-11 17:10:30 +0200

    Removing the license statements from the files in pypopsyn/learning/trainers.

--- pypopsyn/learning/trainers/trainer_base.py
+++ pypopsyn/learning/trainers/trainer_base.py
@@ -1,11 +1,9 @@
-""" Base Trainer.
+"""
+    Base Trainer.
 
     Authors:
 
         Alberto Garcia Garcia (garciagarcia@ice.csic.es)
-
-    Copyright (c) MAGNESIA (ICE-CSIC)
-
 """
 
 import json
--- pypopsyn/learning/trainers/trainers.py
+++ pypopsyn/learning/trainers/trainers.py
@@ -1,1 +1,11 @@
+"""
+    Trainers.
+
+    This is just an empty module that gathers all the available modules.
+
+    Authors:
+
+        Alberto Garcia Garcia (garciagarcia@ice.csic.es)
+"""
+
 from .trainer_basic import TrainerBasic
