a97a4c449fcee0e6bf2dd212d6f10190b0ffe03f
celsa <pardoaraujocelsa@gmail.com>
2024-07-25 10:28:04 +0200

    Addressing last comment from Vanessa.

--- pypopsyn/learning/configuration_parser.py
+++ pypopsyn/learning/configuration_parser.py
@@ -195,12 +195,12 @@ class ConfigurationParser:
             (OrderedDict): The updated configuration dictionary.
         """
 
-        def _apply_update(k: str, v: str) -> None:
+        def _apply_update(k: str, v: Any) -> None:
             """
             Updates a nested dictionary using a semi-colon separated key string.
 
             Args:
-                k (str): A semi-colon separated string representing the keys to traverse in the dictionary.
+                k (str): A semicolon separated string representing the keys to traverse in the dictionary.
                 v (Any): The value to set at the specified location in the dictionary.
             """
             if v is not None:
