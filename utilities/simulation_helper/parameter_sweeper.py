1e3eb6da9adb297f551297828147e7ba4cb350cd
celsa <pardoaraujocelsa@gmail.com>
2024-07-30 18:29:40 +0200

    Adding to the parameter_swweeper.py the variables for the luminosity.

--- utilities/simulation_helper/parameter_sweeper.py
+++ utilities/simulation_helper/parameter_sweeper.py
@@ -252,7 +252,24 @@ if __name__ == "__main__":
         "with number of values [low, high, n_values]."
         "In random mode: range of the power-law slope of the late time magnetic field evolution [low, high].",
     )
-
+    args.add_argument(
+        "--epsilon_L",
+        nargs="*",
+        type=float,
+        default=None,
+        help="In grid mode: range for the power-law slope of the intrinsic luminosity "
+        "with number of values [low, high, n_values]."
+        "In random mode: range of the power-law slope of the intrinsic luminosity [low, high].",
+    )
+    args.add_argument(
+        "--L_radio_log10_mean",
+        nargs="*",
+        type=float,
+        default=None,
+        help="In grid mode: range for the mean of the log-normally distributed radio luminosity normalization factor."
+        "with number of values [low, high, n_values]."
+        "In random mode: range of the mean of the log-normally distributed radio luminosity normalization factor [low, high].",
+    )
     args = args.parse_args()
 
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
