bf68cdaf5f7dd82959fd36191968a7514921f580
Vanessa Graber <graber@ice.csic.es>
2024-08-06 15:50:51 +0100

    Removing unnecessary empty lines.

--- pypopsyn/learning/losses/loss_base.py
+++ pypopsyn/learning/losses/loss_base.py
@@ -12,21 +12,18 @@ import torch
 
 
 class LossBase:
-
     """Base abstract class for all losses."""
 
     @abc.abstractmethod
     def __call__(
         self, output: torch.Tensor, target: torch.Tensor
     ) -> torch.Tensor:
-
         """Actual computation of the loss function."""
 
         raise NotImplementedError
 
     @abc.abstractmethod
     def __str__(self) -> str:
-
         """String representation of the model."""
 
         raise NotImplementedError
