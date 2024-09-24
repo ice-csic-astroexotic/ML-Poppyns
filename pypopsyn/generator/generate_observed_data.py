cd346cbdc92ae2172be21f162e25b251400d18ef
Vanessa Graber <graber@ice.csic.es>
2024-09-24 10:29:52 +0200

    Adding a missing space.

--- pypopsyn/generator/generate_observed_data.py
+++ pypopsyn/generator/generate_observed_data.py
@@ -235,7 +235,7 @@ def generate_dataset(args: argparse.Namespace) -> None:
     df_atnf = df_atnf[~df_atnf["P1"].isin(["NAN"])]
     df_atnf = df_atnf[~df_atnf["ASSOC"].str.match("|".join(discard))]
 
-    # Select only isolated, non-recycled neutron stars, i.e., those with Pdot > 1e-19 and P> 0.01 s.
+    # Select only isolated, non-recycled neutron stars, i.e., those with Pdot > 1e-19 and P > 0.01 s.
     df_atnf = df_atnf[df_atnf["P1"].to_numpy().astype(np.float64) > 1.0e-19]
     df_atnf = df_atnf[df_atnf["P0"].to_numpy().astype(np.float64) > 0.01]
 
