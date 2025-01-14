064b015cb1b1ccb016a13f7da4fda70dac6af100
celsa <pardoaraujocelsa@gmail.com>
2025-01-14 15:32:59 +0100

    Fixing a bug related to the devices.

--- pypopsyn/learning/train_snle.py
+++ pypopsyn/learning/train_snle.py
@@ -436,7 +436,8 @@ def prepare_dataset_sbi(
         x_embbeded = (
             neural_net._embedding_net(torch.tensor(x)).detach().numpy()
         )
-        matrix[i] = (x_embbeded - np.mean(x_embbeded)) / (np.std(x_embbeded))
+        matrix_std = (x_embbeded - np.mean(x_embbeded)) / (np.std(x_embbeded))
+        matrix[i] = torch.tensor(matrix_std, device="cuda")
         parameter[i] = theta
 
     # Transforming the maps and labels into torch.tensors.
