be9d8cc39ffd87fabf3cbaa4d538dadcde2aef5e
celsa <pardoaraujocelsa@gmail.com>
2025-01-15 16:31:23 +0100

    Removing the density_estimator input from the buil_posterior module due to conflicts between devices.

--- pypopsyn/learning/train_snle.py
+++ pypopsyn/learning/train_snle.py
@@ -625,9 +625,8 @@ def amortized_posterior(
             )
 
         posterior = inference.build_posterior(
-            density_estimator,
-            mcmc_method="slice_np",
-            mcmc_parameters={"num_chains": 30, "thin": 5},
+            mcmc_method="slice_np_vectorized",
+            mcmc_parameters={"num_chains": 20, "thin": 5},
         )
         posteriors_list.append(posterior)
 
