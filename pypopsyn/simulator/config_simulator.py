2874a735deb850ec9d0740f94868cd405ad029ca
Vanessa Graber <Vanessa.Graber@rhul.ac.uk>
2025-04-10 16:41:29 +0100

    Switching back to the new luminosity prescription of Pardo et al. (2025) and the updated AAstar parameter files.

--- pypopsyn/simulator/config_simulator.py
+++ pypopsyn/simulator/config_simulator.py
@@ -149,12 +149,12 @@ cfg["P_initial_mean"]: float = 0.3
 cfg["P_initial_sigma"]: float = 0.2
 
 # Mean and standard deviation for the log-normal distributed initial periods in [s].
-cfg["P_initial_log10_mean"]: float = -1.00
-cfg["P_initial_log10_sigma"]: float = 0.38
+cfg["P_initial_log10_mean"]: float = -0.67
+cfg["P_initial_log10_sigma"]: float = 0.55
 
 # Mean and standard deviation for the log-normally distributed initial magnetic fields in [s].
-cfg["B_initial_log10_mean"]: float = 13.10
-cfg["B_initial_log10_sigma"]: float = 0.45
+cfg["B_initial_log10_mean"]: float = 13.09
+cfg["B_initial_log10_sigma"]: float = 0.50
 
 # Dimensionless coefficients k_0, k_1, k_2 for a force-free magnetosphere
 # taken from Spitkovsky (2006) and Philippov et al. (2014).
@@ -196,7 +196,7 @@ cfg["b2"]: float = -0.2
 cfg["tau_late"]: float = 2.0e6
 
 # Late time power-law index.
-cfg["a_late"]: float = -1.80
+cfg["a_late"]: float = -0.88
 
 # Parameters for a log-normal distribution of the magnetic fields of the old millisecond pulsars.
 cfg["B_millisec_mean"] = 8.5
@@ -227,21 +227,21 @@ cfg["a_beam"] = -0.5  # Power-law exponent.
 # (pdf_luminosity_radio_edot). If the luminosity is given by pdf_luminosity_radio_ppdot, then the units of
 # L_radio_log10_mean are [erg s^(3 * epsilon_L - 1)]. Otherwise, L_0 has units of [ergs/s]. In both cases,
 # the following parameters were adjusted to match observed data.
-# cfg[
-#     "L_radio_log10_mean"
-# ]: float = 26.17  # [erg s^(- 1)] if pdf_luminosity_radio_edot is used.
 cfg[
     "L_radio_log10_mean"
-]: float = (
-    35.5  # [erg s^(3 * epsilon_L - 1) ] if pdf_luminosity_radio_ppdot is used.
-)
+]: float = 26.17  # [erg s^(- 1)] if pdf_luminosity_radio_edot is used.
+# cfg[
+#     "L_radio_log10_mean"
+# ]: float = (
+#     35.5  # [erg s^(3 * epsilon_L - 1) ] if pdf_luminosity_radio_ppdot is used.
+# )
 cfg["L_radio_log10_sigma"]: float = 0.8
-cfg["epsilon_L"]: float = 0.5
+cfg["epsilon_L"]: float = 0.68
 cfg["Erot_dot_0"]: float = 1e29
 
 # Spectral index following a normal distribution as in Posselt et al. (2023). We set the standard deviation to 0 to
 # efficiently produce a fixed spectral index.
-cfg["mean_spectral_index"] = -1.6
+cfg["mean_spectral_index"] = -1.8
 cfg["std_spectral_index"] = 0
 
 # Free electron density model for the Galaxy, choose between "ne2001" and "ymw16".
--- pypopsyn/simulator/multiband_emission/emission_radio.py
+++ pypopsyn/simulator/multiband_emission/emission_radio.py
@@ -419,8 +419,8 @@ def calculate_radio_emission_full(
     # Choose one of the two implementations either based on the P and Pdot or the Edot dependence.
     # NOTE: If the luminosity law is changed, the normalisation constant (L_radio_log10_sigma) has to be adjusted in
     # the simulator configuration file pypopsyn/simulator/config_simulator.
-    L_radio_bol = pdf_luminosity_radio_ppdot(P, P_dot)
-    # L_radio_bol = pdf_luminosity_radio_edot(P, P_dot)
+    # L_radio_bol = pdf_luminosity_radio_ppdot(P, P_dot)
+    L_radio_bol = pdf_luminosity_radio_edot(P, P_dot)
 
     # Determining the radio beam angular aperture.
     rho_beam = beam_aperture(P)
