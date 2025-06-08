442a29a84549dc11febcd57418f8dab1917a080d
Vanessa Graber <Vanessa.Graber@rhul.ac.uk>
2025-06-08 09:47:03 +0100

    Updating the NS birth rate to be 2, i.e., 4e7 stars for the 2e9yrs t_max.

--- pypopsyn/simulator/config_simulator.py
+++ pypopsyn/simulator/config_simulator.py
@@ -71,7 +71,7 @@ cfg["resolution"]: int = 10000
 
 # Integer number of neutron stars for the population.
 # Used only when running simulate_population_full.py and simulate_population_dyn.py.
-cfg["NS_number"]: int = 30000000
+cfg["NS_number"]: int = 40000000
 
 # Minimum and maximum ages for the neutron stars in [yr].
 cfg["t_age_min"]: float = 1.0
