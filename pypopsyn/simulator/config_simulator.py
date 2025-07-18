4a6e67c7968b8cee532f7483f348df562dcc55f8
Vanessa Graber <Vanessa.Graber@rhul.ac.uk>
2025-07-18 10:13:42 +0100

    Correcting the formatting of text blocks.

--- pypopsyn/simulator/config_simulator.py
+++ pypopsyn/simulator/config_simulator.py
@@ -475,14 +475,13 @@ cfg["ISM_abundances"]: List[float] = [
 # To obtain the number of Galactic isolated neutron stars detected with a quiescent thermal emission in X-rays,
 # we only include magnetars and XDINSs, as young RPPs with quiescent X-ray emission are primarily discovered through the
 # detection of the supernova remnant emission and pulsar wind nebula contribution which we are not modeling in our code.
-# We currently implement two different kinds of surveys in the X-rays.
-# The first survey only takes into account a simple cut-off in flux. The second one tries to better match the observed
-# distribution and considers that many magnetars have been discovered during an outburst phase. For those we consider
-# a deeper survey to detect a quiescent emission. We then combine it with a shallower survey which detects only the
-# brightest sources.
-# Moreover, since we do not have full control on the observational biases for X-ray surveys, we try to match only the number
-# of sources above a flux threshold of 2e-11 erg s^-1 cm^-2 which defines the limit where we assume real surveys are complete
-# (by looking at the logN-lgS distribution).
+# We currently implement two different kinds of surveys in the X-rays. The first survey only takes into account a simple
+# cut-off in flux. The second one tries to better match the observed distribution and considers that many magnetars have
+# been discovered during an outburst phase. For those we consider a deeper survey to detect a quiescent emission. We
+# then combine it with a shallower survey which detects only the brightest sources. Moreover, since we do not have full
+# control over the observational biases for X-ray surveys, we try to match only the number of sources above a flux
+# threshold of 2e-11 [erg s^-1 cm^-2] which defines the limit where we assume real surveys are complete (by looking at
+# the logN-lgS distribution).
 cfg["surveys_xray"]: dict = {
     "xray_flux_threshold": {
         "path": "pypopsyn/simulator/multiband_surveys/xray_flux_threshold_parameters.json",
