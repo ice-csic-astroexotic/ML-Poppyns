f8f2b0573ece0f3fb6a5cca6020535c9eef36f85
Vanessa Graber <Vanessa.Graber@rhul.ac.uk>
2025-07-31 09:05:26 +0100

    Removing some unnecessary brackets.

--- pypopsyn/simulator/config_simulator.py
+++ pypopsyn/simulator/config_simulator.py
@@ -481,8 +481,8 @@ cfg["ISM_abundances"]: List[float] = [
 # Enabling the use of an interpolator for the rate of crust failures to estimate the number of outburst events for
 # neutron stars with different initial magnetic fields and ages. If set to False a simpler probabilistic approach
 # will be used that takes into account the age but neglects the dependence of the outburst rate on the field strength
-# and configuration. (See the method outburst_filter_probabilistic in
-# pypopsyn/simulator/multiband_emission/emission_xray.py for more details).
+# and configuration. See the method outburst_filter_probabilistic in
+# pypopsyn/simulator/multiband_emission/emission_xray.py for more details.
 cfg["use_crust_failure_rate_interpolator"]: bool = True
 
 # ===================== X-RAY DETECTION PARAMETERS ========================
