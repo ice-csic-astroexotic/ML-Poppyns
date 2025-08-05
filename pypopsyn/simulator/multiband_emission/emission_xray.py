fb62c740f9785973691daf7dcc6edee180efd5a5
MicheleRonchi <mike.29gm@gmail.com>
2025-08-05 11:00:26 +0200

    Some updates to the docstrings to address Vanessa's comments.

--- pypopsyn/simulator/multiband_emission/emission_xray.py
+++ pypopsyn/simulator/multiband_emission/emission_xray.py
@@ -483,8 +483,8 @@ def outburst_filter_probabilistic(
     magnetic energy, effectively assuming that all neutron stars with initial poloidal dipolar fields above 10^13 G are
     born with the same magnetic energy in the crust, resulting in the same number of outbursts. In the magneto-thermal
     models we consider we assume configurations where the magnetic energy increases with the poloidal dipole strength.
-    In particular we assume that the poloidal and toroidal dipoles have always the same strengths and in the models
-    with quadrupole, the quadrupole is always stronger then the dipole by a fixed ratio. Therefore this simplification
+    In particular, we assume that the poloidal and toroidal dipoles have always the same strengths and in the models
+    with quadrupole, the quadrupole is always stronger than the dipole by a fixed ratio. Therefore, this simplification
     is not completely consistent with the models we use.
 
     Args:
